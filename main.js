f937f192ca923909ccf40566cc6f91bfe5818609
zlwym <119268153+zlwym@users.noreply.github.com>
2025-10-01 23:52:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("main.js is connected and running!");
+});
 
-  // Example: highlight nav links when clicked
-//   const navLinks = document.querySelectorAll("nav a");
-//   navLinks.forEach(link => {
-//     link.addEventListener("click", () => {
-//       // Remove highlight from all links
-//       navLinks.forEach(l => l.classList.remove("active"));
-//       // Add highlight to the clicked one
-//       link.classList.add("active");
-//     });
-//   });
+const myName = document.getElementById("name");
+
+myName.addEventListener("click", function(){
+    window.location.href = "index.html";
 });
